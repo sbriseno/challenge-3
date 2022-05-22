25df926e6f6949ac4f63ac8c477ecbac0b3255ce
sergio <sgbriseno95@gmail.com>
2022-05-22 16:16:54 -0500

    running code

--- challenge-3/script.js
+++ challenge-3/script.js
@@ -1,22 +1,26 @@
 // Assignment code here
-var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numbers = "0123456789";
-var special = "!@#$%^&*_+-=";
-const range = (8,128)
+var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*_+-=";
+var range = (9,128);
+var password = "";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 var generatePassword
 function generatePassword() {
+for (var i=0; i<=range; i++) {
+    var randomNumber= Math.floor(Math.random() * chars.length);
+    password += chars.substring(randomNumber, randomNumber +1);
+}
   
-  
+document.getElementById("password").value = password;
 
 }
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector("password");
 
   passwordText.value = password;
 
@@ -24,3 +28,6 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword) 
+function Reset(){
+  document.querySelector("Reset").Reset
+}
